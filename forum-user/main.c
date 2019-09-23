a8c9456a44887f372428dbf0d6d6e22b48c5e87d
QuickSoOrt <brunorafaelfariameira@gmail.com>
2019-09-24 00:21:19 +0100

    server semi-done

---
+++ forum-user/main.c
@@ -0,0 +1,193 @@
+#include <stdio.h>
+#include <netdb.h>
+#include <errno.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <arpa/inet.h>
+#include <sys/types.h>
+#include <sys/select.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+
+#define BUFFER_SIZE 128
+#define ASCII_LIMIT 256
+#define MAX_ARGS_N  3
+#define MAX_ARGS_L  128
+
+enum param_types {
+	PARAM_FSIP    = (unsigned char)'n',
+	PARAM_FSPORT  = (unsigned char)'p',
+};
+
+static void set_default_params(char main_params[ASCII_LIMIT][BUFFER_SIZE]) {
+	strcpy(main_params[PARAM_FSIP],   "xsy");
+	strcpy(main_params[PARAM_FSPORT], "58000");
+}
+
+static void concatenate_args(char *buffer, char* msg, char args[MAX_ARGS_N][MAX_ARGS_L], int num_tokens) {
+	int i;
+	buffer[0] = '\0';
+	strcpy(buffer, msg);
+	for (i = 0; i < num_tokens - 1; i++) {
+		strcat(buffer, " ");
+		strcat(buffer, args[i + 1]);
+	}
+	strcat(buffer, "\n\0");
+}
+
+static void executes_tcp_command(char *buffer, struct addrinfo *res_tcp) {
+	int server_sock_tcp, error_code;
+
+	/* Socket TCP creation */
+	server_sock_tcp = socket(res_tcp->ai_family, res_tcp->ai_socktype, res_tcp->ai_protocol);
+
+	if (server_sock_tcp == -1) {
+		fprintf(stderr, "socket creation failed: %s\n", strerror(errno));
+		exit(EXIT_FAILURE);
+	}
+	/*			           */
+
+	/* Connect to TCP socket */
+	error_code = connect(server_sock_tcp, res_tcp->ai_addr, res_tcp->ai_addrlen);
+
+	if (error_code == -1) {
+		fprintf(stderr, "connect failed: %s\n", strerror(errno));
+		exit(EXIT_FAILURE);
+	}
+
+	/* Write on server socket */
+	write(server_sock_tcp, buffer, BUFFER_SIZE);
+
+	/* Read from server socket */
+	read(server_sock_tcp, buffer, BUFFER_SIZE);
+
+	close(server_sock_tcp);
+}
+
+int main(int argc, char const *argv[])
+{
+	int 				server_sock_udp, error_code, num_tokens;
+	struct addrinfo 	hints, *res_udp, *res_tcp;
+	char				buffer[BUFFER_SIZE], *buffer_ptr, main_params[ASCII_LIMIT][BUFFER_SIZE], args[MAX_ARGS_N][MAX_ARGS_L];
+	ssize_t 			n;
+
+	set_default_params(main_params);
+
+	/* Initialization of hints structure for socket UDP */
+	memset((void *) &hints, 0, sizeof(struct addrinfo));
+	hints.ai_family   = AF_INET;
+	hints.ai_socktype = SOCK_DGRAM;
+	hints.ai_flags    = AI_NUMERICSERV;
+	/*									                */
+
+	error_code = getaddrinfo(main_params[PARAM_FSIP], main_params[PARAM_FSPORT], &hints, &res_udp);
+
+	if (error_code != 0) {
+		fprintf(stderr, "getaddrinfo failed: %s\n", gai_strerror(error_code));
+		exit(EXIT_FAILURE);
+	}
+
+	/* Socket UDP creation */
+	server_sock_udp = socket(res_udp->ai_family, res_udp->ai_socktype, res_udp->ai_protocol);
+
+	if (server_sock_udp == -1) {
+		fprintf(stderr, "socket creation failed: %s\n", strerror(errno));
+		exit(EXIT_FAILURE);
+	}
+	/*			           */
+
+	/* Initialization of hints structure for socket TCP */
+	hints.ai_socktype = SOCK_STREAM;
+	/*                                                  */
+		
+	error_code = getaddrinfo(main_params[PARAM_FSIP], main_params[PARAM_FSPORT], &hints, &res_tcp);
+
+	if (error_code != 0) {
+		fprintf(stderr, "getaddrinfo failed: %s\n", gai_strerror(error_code));
+		exit(EXIT_FAILURE);
+	}
+
+	printf("Enter command (? for help) : ");
+
+	while (1) {
+		/* reads from terminal */
+		buffer_ptr = fgets(buffer, BUFFER_SIZE, stdin);
+		if (buffer_ptr == NULL) {
+			fprintf(stderr, "fgets failed\n");
+			exit(EXIT_FAILURE);
+		}
+		/*                     */
+
+		/* executes command */
+		num_tokens = sscanf(buffer, "%s %s %s %s %s", args[0], args[1], args[2], args[3], args[4]);
+
+		if (!strcmp(args[0], "register")             || 
+			!strcmp(args[0], "reg")) {
+				concatenate_args(buffer, "REG", args, num_tokens);
+				sendto(server_sock_udp, buffer, strlen(buffer), 0, (struct sockaddr *) res_udp->ai_addr, res_udp->ai_addrlen);
+		}
+		else if (!strcmp(args[0], "topic_list")      || 
+			     !strcmp(args[0], "tl")) {
+				concatenate_args(buffer, "LTP", args, num_tokens);
+				sendto(server_sock_udp, buffer, strlen(buffer), 0, (struct sockaddr *) res_udp->ai_addr, res_udp->ai_addrlen);
+			
+		}
+		else if (!strcmp(args[0], "topic_select")    || 
+			     !strcmp(args[0], "ts")) {
+				concatenate_args(buffer, "LT", args, num_tokens);
+				sendto(server_sock_udp, buffer, strlen(buffer), 0, (struct sockaddr *) res_udp->ai_addr, res_udp->ai_addrlen);
+			
+		}
+		else if (!strcmp(args[0], "topic_propose")   || 
+			     !strcmp(args[0], "tp")) {
+				concatenate_args(buffer, "PTP", args, num_tokens);
+				sendto(server_sock_udp, buffer, strlen(buffer), 0, (struct sockaddr *) res_udp->ai_addr, res_udp->ai_addrlen);
+
+		}
+		else if (!strcmp(args[0], "question_list")   || 
+			     !strcmp(args[0], "ql")) {
+				concatenate_args(buffer, "LQU", args, num_tokens);
+				sendto(server_sock_udp, buffer, strlen(buffer), 0, (struct sockaddr *) res_udp->ai_addr, res_udp->ai_addrlen);
+			
+		}
+		else if (!strcmp(args[0], "question_get")    || 
+			     !strcmp(args[0], "qg")) {
+				concatenate_args(buffer, "GQU", args, num_tokens);
+				executes_tcp_command(buffer, res_tcp);
+				
+		}
+		else if (!strcmp(args[0], "question_submit") || 
+			     !strcmp(args[0], "qs")) {
+				concatenate_args(buffer, "QUS", args, num_tokens);
+			
+		}
+		else if (!strcmp(args[0], "answer_submit")   || 
+			     !strcmp(args[0], "as")) {
+				concatenate_args(buffer, "ANS", args, num_tokens);
+				
+		}
+		else if (!strcmp(args[0], "exit")) {
+				break;
+		}
+		else if (!strcmp(args[0], "?")) {
+				printf("register/reg <userId>\n" 
+						"topic_list/tl\n"
+						"topic_select <topic name>\n"
+						"ts <topic number>\n"
+						"topic_propose/tp <topic>\n"
+						"question_list/ql\n"
+						"question_get <question name>\n"
+						"qg <question_number>\n"
+						"answer_submit/as <text_file [image_file]>\n"
+						"question_submit/qs <text_file [image_file]>\n"
+						"exit\n");
+		}
+		else {
+				fprintf(stdout, "Invalid command!\n");
+		}
+		/*                  */
+	}
+
+	return 0;
+}
