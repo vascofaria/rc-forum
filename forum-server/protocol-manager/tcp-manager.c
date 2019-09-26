d551e538c795d410a4793c02084c951d5b749844
Vasco Faria <vascoffaria@gmail.com>
2019-09-26 19:58:26 +0100

    all input parsing done

--- forum-server/protocol-manager/tcp-manager.c
+++ forum-server/protocol-manager/tcp-manager.c
@@ -3,25 +3,17 @@
 #include "tcp-manager.h"
 #include "protocol.h"
 #include "../entities/question.h"
-// #include "../exceptions.h"
+#include "../exceptions.h"
 
 #include <stdbool.h>
 
-
-// case GQU
-// char topic[MAXNAME]
-// char question[MAXNAME]
-// parse_i_GQU(request, topic, question)
-// if error_code return parse_output_ERROR()
-// else
-// error_code = question_get(topic, question)
-// if error_code parse_output_ERROR(error_code) from exceptions.h
 char *tcp_manager(char *request) {
 	int  i, error_code;
 	char protocol[PROTOCOL_SIZE + 1];
 	char topic[TOPIC_SIZE + 1];
 	char question_title[QUESTION_SIZE + 1];
-	question_t **question;
+	question_t *question;
+	answer_t *answer;
 
 
 	for (i = 0; i < PROTOCOL_SIZE && request[i] != '\0' && request[i] != '\n';  i++) {
@@ -33,24 +25,29 @@ char *tcp_manager(char *request) {
 		error_code = parse_input_GQU(request, topic, question_title);
 		if (error_code) {
 			printf("ERROR on GQU\n");
+			return parse_output_ERROR(error_code);
 		}
+		// get_question(topic, question_title, &question);
+		return parse_output_QGR(question); -> free_question
+	} else if (!strcmp(protocol, QUS)) {
+		error_code = parse_input_QUS(request, topic, &question);
+		if (error_code) {
+			printf("ERROR on QUS\n");
+			return parse_output_ERROR(error);
+		}
+		// question_submit(topic, question) -> free_question()
+		return parse_output_QUR()
+	} else if (!strcmp(protocol, ANS)) {
+		error_code = parse_input_ANS(request, topic, question_title, &answer);
+		if (error_code) {
+			printf("ERROR on ANS\n");
+			return parse_output_ERROR(error_code);
+		}
+		// answer_submit(topic, question_title, answer) -> free_answer()
+		return parse_output_ANR()
 	}
 
-
-	// switch
-
-	// case GQU
-	// parse_input_GQU, question_get, parse_output_QGR
-
-	// case QUS
-	// parse_input_QUS, question_submit, parse_output_QUR
-
-	// case ANS
-	// parse_input_ANS, answer_submit, parse_output_ANR
-
-	// case default
-	// parse_output_ERROR
-	return NULL;
+	return parse_output_ERROR(BAD_INPUT);
 }
 
 /*
@@ -94,7 +91,7 @@ static char *parse_output_QGR(question_t *question) {
 /*
  * QUESTION SUBMIT
 */
-static int parse_input_QUS(char *request, char *topic, question_t *question) {
+static int parse_input_QUS(char *request, char *topic, question_t **question) {
 	int  i, j;
 	int  question_size = 0;
 	char question_user_id[MAX_TITLE+1];
@@ -117,6 +114,10 @@ static int parse_input_QUS(char *request, char *topic, question_t *question) {
 	}
 	question_user_id[j] = '\0';
 
+	if (request[i++] != ' ') {
+		return BAD_INPUT;
+	}
+
 	for (j = 0; j < TOPIC_SIZE && request[i] != '\0' && request[i] != '\n'; i++, j++) {
 		if (request[i] == ' ') {
 			return BAD_INPUT;
@@ -125,6 +126,10 @@ static int parse_input_QUS(char *request, char *topic, question_t *question) {
 	}
 	topic[j] = '\0';
 
+	if (request[i++] != ' ') {
+		return BAD_INPUT;
+	}
+
 	for (j = 0; j < QUESTION_TITLE_SIZE && request[i] != '\0' && request[i] != '\n'; i++, j++) {
 		if (request[i] == ' ') {
 			return BAD_INPUT;
@@ -132,6 +137,10 @@ static int parse_input_QUS(char *request, char *topic, question_t *question) {
 		question_title[i] = request[i];
 	}
 	question_title[i] = '\0';
+
+	if (request[i++] != ' ') {
+		return BAD_INPUT;
+	}
 
 	for (j = 0; request[i] != '\0' && request[i] != '\n' && request[i] != ' '; i++, j++) {
 		if (request[i] < '0' || request[i] > '9') {
@@ -145,9 +154,14 @@ static int parse_input_QUS(char *request, char *topic, question_t *question) {
 	}
 	question_size /= 10;
 
-	for (j = 0; j < question_size && j < QUESTION_SIZE && request[i] != '\0' && request[i] != '\n'; i++, j++) {
+	if (request[i++] != ' ') {
+		return BAD_INPUT;
+	}
+
+	for (j = 0; j < question_size && j < QUESTION_SIZE && request[i] != '\0'; i++, j++) {
 		question_data[j] = request[i];
 	}
+	question_data[j] = '\0';
 
 	if (request[i++] != ' ' || j != question_size) {
 		return BAD_INPUT;
@@ -187,15 +201,16 @@ static int parse_input_QUS(char *request, char *topic, question_t *question) {
 			return BAD_INPUT;
 		}
 
-		for (j = 0; j < question_img_size && j < IMAGE_SIZE && request[i] != '\0' && request[i] != '\n'; i++, j++) {
+		for (j = 0; j < question_img_size && j < IMAGE_SIZE && request[i] != '\0'; i++, j++) {
 			question_img_data[j] = request[i];
 		}
+		question_img_data[j] = '\0';
 
 		if (request[i] != ' ' && request[i+1] != '\0') {
 			return BAD_INPUT;
 		}
 
-		question = new_question(question_title, question_size, question_data, question_img_size, question_img_data, NULL);
+		*question = new_question(question_title, question_size, question_data, question_img_size, question_img_data, NULL);
 		return SUCCESS;
 
 	} else if (request[i++] == '0') {
@@ -203,7 +218,7 @@ static int parse_input_QUS(char *request, char *topic, question_t *question) {
 			return BAD_INPUT;
 		}
 
-		question = new_question(question_title, question_size, question_data, 0, NULL, NULL);
+		*question = new_question(question_title, question_size, question_data, 0, NULL, NULL);
 		return SUCCESS;
 	}
 
@@ -218,9 +233,137 @@ static char *parse_output_QUR() {
 /*
  * ANSWER SUBMIT
 */
-static int parse_input_ANS(char *request) {
-	// check the request string
-	return 0;
+static int parse_input_ANS(char *request, char *topic, char *question_title, answer_t **answer) {
+	int  i, j;
+	char answer_user_id[MAX_TITLE+1];
+	char answer_title[ANSWER_TITLE_SIZE+1];
+	int  answer_size = 0;
+	char answer_data[ANSWER_SIZE+1];
+	bool has_answer_img;
+	int  answer_img_size = 0;
+	char answer_img_ext[IMAGE_EXT_SIZE+1];
+	char answer_img_data[IMAGE_SIZE+1];
+
+	if (request[PROTOCOL_SIZE] != ' ') {
+		return BAD_INPUT;
+	}
+
+	for (i = PROTOCOL_SIZE + 1, j = 0; j < USER_ID_SIZE && request[i] != '\0' && request[i] != '\n'; i++, j++) {
+		if (request[i] < '0' || request[i] > '9') {
+			return BAD_INPUT;
+		}
+		question_user_id[j] = request[i];
+	}
+	question_user_id[j] = '\0';
+
+	if (request[i++] != ' ') {
+		return BAD_INPUT;
+	}
+
+	for (j = 0; j < TOPIC_SIZE && request[i] != '\0' && request[i] != '\n'; i++, j++) {
+		if (request[i] == ' ') {
+			return BAD_INPUT;
+		}
+		topic[j] = request[i];
+	}
+	topic[j] = '\0';
+
+	if (request[i++] != ' ') {
+		return BAD_INPUT;
+	}
+
+	for (j = 0; j < QUESTION_TITLE_SIZE && request[i] != '\0' && request[i] != '\n'; i++, j++) {
+		if (request[i] == ' ') {
+			return BAD_INPUT;
+		}
+		question_title[i] = request[i];
+	}
+	question_title[i] = '\0';
+
+	if (request[i++] != ' ') {
+		return BAD_INPUT;
+	}
+
+	for (j = 0; request[i] != '\0' && request[i] != '\n' && request[i] != ' '; i++, j++) {
+		if (request[i] < '0' || request[i] > '9') {
+			return BAD_INPUT;
+		}
+		answer_size += request[i] - '0';
+		answer_size *= 10;
+	}
+	if (answer_size == 0) {
+		return BAD_INPUT;
+	}
+	answer_size /= 10;
+
+	if (request[i++] != ' ') {
+		return BAD_INPUT;
+	}
+
+	for (j = 0; j < answer_size && j < QUESTION_SIZE && request[i] != '\0'; i++, j++) {
+		answer_data[j] = request[i];
+	}
+	answer_data[j] = '\0';
+
+	if (request[i++] != ' ' || j != answer_size) {
+		return BAD_INPUT;
+	}
+
+	if (request[i++] == '1') {
+		if (request[i++] != ' ') {
+			return BAD_INPUT;
+		}
+
+		for (j = 0; j < IMAGE_EXT_SIZE && request[i] != '\0' && request[i] != '\n'; i++, j++) {
+			if (request[i] == ' ') {
+				return BAD_INPUT;
+			}
+			answer_img_ext[j] = request[i];
+		}
+		answer_img_ext[j] = '\0';
+
+		if (request[i++] != 0) {
+			return BAD_INPUT;
+		}
+
+		for (j = 0; request[i] != '\0' && request[i] != '\n' && request[i] != ' '; i++, j++) {
+			if (request[i] < '0' || request[i] > '9') {
+				return BAD_INPUT;
+			}
+			answer_img_size += request[i] - '0';
+			answer_img_size *= 10;
+		}
+		if (answer_img_size == 0) {
+			return BAD_INPUT;
+		}
+		answer_img_size /= 10;
+
+		if (request[i++] != ' ') {
+			return BAD_INPUT;
+		}
+
+		for (j = 0; j < answer_img_size && j < IMAGE_SIZE && request[i] != '\0'; i++, j++) {
+			answer_img_data[j] = request[i];
+		}
+		answer_img_data[j] = '\0';
+
+		if (request[i] != ' ' && request[i+1] != '\0') {
+			return BAD_INPUT;
+		}
+
+		*answer = new_answer(answer_title, answer_size, answer_data, answer_img_size, answer_img_data, NULL);
+		return SUCCESS;
+
+	} else if (request[i++] == '0') {
+		if (request[i] != ' ' && request[i+1] != '\0') {
+			return BAD_INPUT;
+		}
+
+		*answer = new_answer(answer_title, answer_size, answer_data, 0, NULL, NULL);
+		return SUCCESS;
+	}
+
+	return BAD_INPUT;
 }
 
 static char *parse_output_ANR() {
